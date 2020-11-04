711060e6e2accef7611196f39d3a35e95d4a351d
Lucas Cardoso <lucaas@mail.com>
2020-11-04 20:05:08 -0300

    Update CameraView.swift

--- smartdoor/CameraView.swift
+++ smartdoor/CameraView.swift
@@ -45,7 +45,7 @@ final class CameraView: UIView {
         captureSession?.sessionPreset = .high
         //# DEFINE CAMERA FRONTAL
         let defaultVideoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .front)
-    
+        
         
         let input = try! AVCaptureDeviceInput(device: defaultVideoDevice!)
         captureSession?.addInput(input as AVCaptureInput)
@@ -78,7 +78,7 @@ extension CameraView: AVCaptureVideoDataOutputSampleBufferDelegate {
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer),
-            let exifOrientation = CGImagePropertyOrientation(rawValue: 0) else
+              let exifOrientation = CGImagePropertyOrientation(rawValue: 0) else
         {
             return
         }
@@ -102,8 +102,6 @@ extension CameraView: AVCaptureVideoDataOutputSampleBufferDelegate {
     }
     
     
-    
-    
     func triggerService(){
         if(!storeBuffer.shared.serviceBlocked){
             storeBuffer.shared.serviceBlocked = true
@@ -111,7 +109,7 @@ extension CameraView: AVCaptureVideoDataOutputSampleBufferDelegate {
                 storeBuffer.shared.serviceBlocked = false
             }
             if let img = storeBuffer.shared.actBuffer {
-                let uiimageFromBuffer = self.imageFromSampleBuffer(sampleBuffer: img)
+                let uiimageFromBuffer = self.transform(sampleBuffer: img)
                 FaceService().request(img: uiimageFromBuffer) { result in
                 }
             }
@@ -128,7 +126,7 @@ extension CameraView: AVCaptureVideoDataOutputSampleBufferDelegate {
                 mask.removeFromSuperlayer()
             }
             guard results.isEmpty == false else {
-//                self.delegate.cameraViewFoundNoTargets()
+                //                self.delegate.cameraViewFoundNoTargets()
                 return
             }
             DispatchQueue.main.async {
@@ -151,45 +149,58 @@ extension CameraView: AVCaptureVideoDataOutputSampleBufferDelegate {
         }
     }
     
-    func imageFromSampleBuffer(sampleBuffer : CMSampleBuffer) -> UIImage
-    {
-        // Get a CMSampleBuffer's Core Video image buffer for the media data
-        let  imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
-        // Lock the base address of the pixel buffer
-        CVPixelBufferLockBaseAddress(imageBuffer!, CVPixelBufferLockFlags.readOnly);
-        
-        
-        // Get the number of bytes per row for the pixel buffer
-        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer!);
-        
-        // Get the number of bytes per row for the pixel buffer
-        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer!);
-        // Get the pixel buffer width and height
-        let width = CVPixelBufferGetWidth(imageBuffer!);
-        let height = CVPixelBufferGetHeight(imageBuffer!);
-        
-        // Create a device-dependent RGB color space
-        let colorSpace = CGColorSpaceCreateDeviceRGB();
-        
-        // Create a bitmap graphics context with the sample buffer data
-        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Little.rawValue
-        bitmapInfo |= CGImageAlphaInfo.premultipliedFirst.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
-        //let bitmapInfo: UInt32 = CGBitmapInfo.alphaInfoMask.rawValue
-        let context = CGContext.init(data: baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
-        // Create a Quartz image from the pixel data in the bitmap graphics context
-        let quartzImage = context?.makeImage();
-        // Unlock the pixel buffer
-        CVPixelBufferUnlockBaseAddress(imageBuffer!, CVPixelBufferLockFlags.readOnly);
-        
-        // Create an image object from the Quartz image
-        let image = UIImage.init(cgImage: quartzImage!);
-        
-        return (image);
+//    func imageFromSampleBuffer(sampleBuffer : CMSampleBuffer) -> UIImage
+//    {
+//        // Get a CMSampleBuffer's Core Video image buffer for the media data
+//        let  imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+//        // Lock the base address of the pixel buffer
+//        CVPixelBufferLockBaseAddress(imageBuffer!, CVPixelBufferLockFlags.readOnly);
+//        
+//        
+//        // Get the number of bytes per row for the pixel buffer
+//        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer!);
+//        
+//        // Get the number of bytes per row for the pixel buffer
+//        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer!);
+//        // Get the pixel buffer width and height
+//        let width = CVPixelBufferGetWidth(imageBuffer!);
+//        let height = CVPixelBufferGetHeight(imageBuffer!);
+//        
+//        // Create a device-dependent RGB color space
+//        let colorSpace = CGColorSpaceCreateDeviceRGB();
+//        
+//        // Create a bitmap graphics context with the sample buffer data
+//        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Little.rawValue
+//        bitmapInfo |= CGImageAlphaInfo.premultipliedFirst.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
+//        //let bitmapInfo: UInt32 = CGBitmapInfo.alphaInfoMask.rawValue
+//        let context = CGContext.init(data: baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
+//        // Create a Quartz image from the pixel data in the bitmap graphics context
+//        let quartzImage = context?.makeImage();
+//        // Unlock the pixel buffer
+//        CVPixelBufferUnlockBaseAddress(imageBuffer!, CVPixelBufferLockFlags.readOnly);
+//        
+//        // Create an image object from the Quartz image
+//        let image = UIImage.init(cgImage: quartzImage!);
+//        
+//        return (image);
+//    }
+    
+    func transform(sampleBuffer : CMSampleBuffer) -> UIImage {
+        var img = UIImage()
+        if let cvImageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+            let ciimage = CIImage(cvImageBuffer: cvImageBuffer)
+            let context = CIContext()
+            
+            if let cgImage = context.createCGImage(ciimage, from: ciimage.extent) {
+                img = UIImage(cgImage: cgImage)
+            }
+        }
+        return img
     }
     
     func drawFaceBox(index: Int, frame: CGRect) {
         if index == 0 {
-//            delegate.cameraViewDidTarget(frame: frame)
+            //            delegate.cameraViewDidTarget(frame: frame)
             createLayer(in: frame, color: UIColor.green.cgColor)
         } else {
             createLayer(in: frame, color: UIColor.yellow.cgColor)
